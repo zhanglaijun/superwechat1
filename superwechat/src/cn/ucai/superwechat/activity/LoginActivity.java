4cd166e19137a8d3340a0a0fe7af67100ff2cb71
zhanglaijun <125682237@qq.com>
2016-07-20 17:36:41 +0800

    添加下载好有的方法

--- superwechat/src/cn/ucai/superwechat/activity/LoginActivity.java
+++ superwechat/src/cn/ucai/superwechat/activity/LoginActivity.java
@@ -45,6 +45,7 @@ import cn.ucai.superwechat.bean.Result;
 import cn.ucai.superwechat.bean.UserAvatar;
 import cn.ucai.superwechat.db.UserDao;
 import cn.ucai.superwechat.domain.User;
+import cn.ucai.superwechat.task.DownloadContactListTask;
 import cn.ucai.superwechat.utils.CommonUtils;
 import cn.ucai.superwechat.utils.OkHttpUtils2;
 import cn.ucai.superwechat.utils.Utils;
@@ -223,7 +224,7 @@ public class LoginActivity extends BaseActivity {
 		SuperWeChatApplication.getInstance().setUser(user);
 		SuperWeChatApplication.currentUserNick=user.getMUserNick();
 
-
+        new DownloadContactListTask(LoginActivity.this,currentUsername).execute();
 		try {
 			// ** 第一次登录或者之前logout后再登录，加载所有本地群和回话
 			// ** manually load all local groups and
