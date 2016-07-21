d620830b3e798dff05831da99f2cf5b654b858ae
zhanglaijun <125682237@qq.com>
2016-07-21 15:05:26 +0800

    根据username获取相应的user

--- superwechat/src/cn/ucai/superwechat/utils/UserUtils.java
+++ superwechat/src/cn/ucai/superwechat/utils/UserUtils.java
@@ -5,33 +5,47 @@ import android.text.TextUtils;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import cn.ucai.superwechat.SuperWeChatApplication;
 import cn.ucai.superwechat.applib.controller.HXSDKHelper;
 import cn.ucai.superwechat.DemoHXSDKHelper;
 
 import cn.ucai.superwechat.R;
+import cn.ucai.superwechat.bean.UserAvatar;
 import cn.ucai.superwechat.domain.User;
 import com.squareup.picasso.Picasso;
 
 public class UserUtils {
     /**
-     * 根据username获取相应user，由于demo没有真实的用户数据，这里给的模拟的数据；
-     * @param username
-     * @return
-     */
-    public static User getUserInfo(String username){
-        User user = ((DemoHXSDKHelper) HXSDKHelper.getInstance()).getContactList().get(username);
-        if(user == null){
-            user = new User(username);
-        }
-            
-        if(user != null){
-            //demo没有这些数据，临时填充
-        	if(TextUtils.isEmpty(user.getNick()))
-        		user.setNick(username);
-        }
-        return user;
+		 * 根据username获取相应user，由于demo没有真实的用户数据，这里给的模拟的数据；
+		 * @param username
+		 * @return
+		 */
+	public static User getUserInfo(String username){
+		User user = ((DemoHXSDKHelper) HXSDKHelper.getInstance()).getContactList().get(username);
+		if(user == null){
+			user = new User(username);
+		}
+
+		if(user != null){
+			//demo没有这些数据，临时填充
+			if(TextUtils.isEmpty(user.getNick()))
+				user.setNick(username);
+		}
+		return user;
     }
-    
+	/**
+	 * 根据username获取相应user，由于demo没有真实的用户数据，这里给的模拟的数据；
+	 * @param username
+	 * @return
+	 */
+	public static UserAvatar getAppUserInfo(String username) {
+		UserAvatar user = SuperWeChatApplication.getInstance().getUserMap().get(username);
+		if (user == null) {
+			user = new UserAvatar();
+		}
+
+		return user;
+	}
     /**
      * 设置用户头像
      * @param username
@@ -68,7 +82,24 @@ public class UserUtils {
     		textView.setText(username);
     	}
     }
-    
+    /**
+     * 设置用户昵称
+     */
+    public static void setAppUserNick(String username,TextView textView){
+    	UserAvatar user = getAppUserInfo(username);
+    	if(user != null){
+			if(user.getMUserNick()!=null){
+
+				textView.setText(user.getMUserNick());
+			}
+			else {
+				textView.setText(username);
+			}
+    	}else{
+    		textView.setText(username);
+    	}
+    }
+
     /**
      * 设置当前用户昵称
      */
