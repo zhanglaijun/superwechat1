b4982601f17e2a52e57285e4dd01c40131298a28
zhanglaijun <125682237@qq.com>
2016-07-22 15:13:57 +0800

    添加好友头像设置

--- superwechat/src/cn/ucai/superwechat/adapter/NewFriendsMsgAdapter.java
+++ superwechat/src/cn/ucai/superwechat/adapter/NewFriendsMsgAdapter.java
@@ -13,13 +13,12 @@
  */
 package cn.ucai.superwechat.adapter;
 
-import java.util.List;
-
 import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.ContentValues;
 import android.content.Context;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
@@ -32,12 +31,21 @@ import android.widget.Toast;
 
 import com.easemob.chat.EMChatManager;
 import com.easemob.chat.EMGroupManager;
+
+import java.util.List;
+
+import cn.ucai.superwechat.I;
 import cn.ucai.superwechat.R;
+import cn.ucai.superwechat.bean.Result;
+import cn.ucai.superwechat.bean.UserAvatar;
 import cn.ucai.superwechat.db.InviteMessgeDao;
 import cn.ucai.superwechat.domain.InviteMessage;
+import cn.ucai.superwechat.utils.OkHttpUtils2;
+import cn.ucai.superwechat.utils.UserUtils;
+import cn.ucai.superwechat.utils.Utils;
 
 public class NewFriendsMsgAdapter extends ArrayAdapter<InviteMessage> {
-
+    private static final String  TAG=NewFriendsMsgAdapter.class.getSimpleName();
 	private Context context;
 	private InviteMessgeDao messgeDao;
 
@@ -123,6 +131,32 @@ public class NewFriendsMsgAdapter extends ArrayAdapter<InviteMessage> {
 			}
 
 			// 设置用户头像
+			UserUtils.setAppUserAvatar(context,msg.getFrom(),holder.avator);
+			final OkHttpUtils2<String>utils2=new OkHttpUtils2<String>();
+			utils2.setRequestUrl(I.REQUEST_FIND_USER)
+					.addParam(I.User.USER_NAME,msg.getFrom())
+					.targetClass(String.class)
+					.execute(new OkHttpUtils2.OnCompleteListener<String>() {
+						@Override
+						public void onSuccess(String s) {
+							Log.e(TAG,"s="+s);
+							Result result= Utils.getResultFromJson(s, UserAvatar.class);
+							Log.e(TAG,"result="+result);
+							if(result!=null&&result.isRetMsg()){
+								UserAvatar user= (UserAvatar) result.getRetData();
+								Log.e(TAG,"user="+user);
+								if(user!=null){
+									UserUtils.setAppUserNick(user,holder.name);
+								}
+							}
+
+						}
+
+						@Override
+						public void onError(String error) {
+							Log.e(TAG,"error="+error);
+						}
+					});
 		}
 
 		return convertView;
@@ -130,9 +164,7 @@ public class NewFriendsMsgAdapter extends ArrayAdapter<InviteMessage> {
 
 	/**
 	 * 同意好友请求或者群申请
-	 * 
 	 * @param button
-	 * @param username
 	 */
 	private void acceptInvitation(final Button button, final InviteMessage msg) {
 		final ProgressDialog pd = new ProgressDialog(context);
@@ -173,7 +205,7 @@ public class NewFriendsMsgAdapter extends ArrayAdapter<InviteMessage> {
 						@Override
 						public void run() {
 							pd.dismiss();
-							Toast.makeText(context, str3 + e.getMessage(), 1).show();
+							Toast.makeText(context, str3 + e.getMessage(), Toast.LENGTH_SHORT).show();
 						}
 					});
 
