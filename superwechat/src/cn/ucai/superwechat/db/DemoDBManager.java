cec9daef853233e6a40602edde70bc3b6bd2da42
zhanglaijun <125682237@qq.com>
2016-07-23 14:03:13 +0800

    更新联系人列表

--- superwechat/src/cn/ucai/superwechat/db/DemoDBManager.java
+++ superwechat/src/cn/ucai/superwechat/db/DemoDBManager.java
@@ -349,7 +349,7 @@ public class DemoDBManager {
 	}
 
     /**
-     * 保存一个联系人
+     * 保存登录用户
      * @param user
      */
     synchronized public void saveUserAvatar(UserAvatar user){
@@ -358,31 +358,29 @@ public class DemoDBManager {
         values.put(UserDao.USER_COLUMN_NAME_ID, user.getMUserName());
         values.put(UserDao.USER_COLUMN_NAME_NICK, user.getMUserNick());
         values.put(UserDao.USER_COLUMN_NAME_AVATAR, user.getMAvatarId());
-        values.put(UserDao.USER_COLUMN_NAME_AVATAR_PAHT, user.getMAvatarPath());
-        values.put(UserDao.USER_COLUMN_NAME_AVATAR_TYPE, user.getMAvatarType());
-        values.put(UserDao.USER_COLUMN_NAME_AVATAR_LAST_UPDATE_TIME, user.getMAvatarLastUpdateTime());
-
+        values.put(UserDao.USER_COLUMN_AVATAR_PATH, user.getMAvatarPath());
+        values.put(UserDao.USER_COLUMN_AVATAR_TYPE, user.getMAvatarType());
+        values.put(UserDao.USER_COLUMN_AVATAR_LAST_UPDATE_TIME, user.getMAvatarLastUpdateTime());
         if(db.isOpen()){
-            db.replace(UserDao.TABLE_NAME, null, values);
+            db.replace(UserDao.USER_TABLE_NAME, null, values);
         }
     }
-    synchronized public UserAvatar getUserAvatar(String username){
-        SQLiteDatabase db=dbHelper.getReadableDatabase();
-        Cursor cursor=db.rawQuery("select * from "+UserDao.USER_TABLE_NAME
-        + "where "+UserDao.USER_COLUMN_NAME_ID +"=?",new String[]{username});
-        UserAvatar user=null;
-        if(cursor.moveToNext()){
-            user=new UserAvatar();
-            user.setMUserName(username);
+
+
+    synchronized public UserAvatar getUserAvatar(String userName) {
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
+        Cursor cursor = db.rawQuery("select * from " + UserDao.USER_TABLE_NAME + " where " + UserDao.USER_COLUMN_NAME_ID + "=?", new String[]{userName});
+        UserAvatar user = null;
+        if (cursor.moveToNext()) {
+            user = new UserAvatar();
+            user.setMUserName(userName);
             user.setMUserNick(cursor.getString(cursor.getColumnIndex(UserDao.USER_COLUMN_NAME_NICK)));
             user.setMAvatarId(cursor.getInt(cursor.getColumnIndex(UserDao.USER_COLUMN_NAME_AVATAR)));
-            user.setMAvatarType(cursor.getInt(cursor.getColumnIndex(UserDao.USER_COLUMN_NAME_AVATAR_TYPE)));
-            user.setMAvatarPath(cursor.getString(cursor.getColumnIndex(UserDao.USER_COLUMN_NAME_AVATAR_PAHT)));
-            user.setMAvatarLastUpdateTime(cursor.getString(cursor.getColumnIndex(UserDao.USER_COLUMN_NAME_AVATAR_LAST_UPDATE_TIME)));
+            user.setMAvatarType(cursor.getInt(cursor.getColumnIndex(UserDao.USER_COLUMN_AVATAR_TYPE)));
+            user.setMAvatarPath(cursor.getString(cursor.getColumnIndex(UserDao.USER_COLUMN_AVATAR_PATH)));
+            user.setMAvatarLastUpdateTime(cursor.getString(cursor.getColumnIndex(UserDao.USER_COLUMN_AVATAR_LAST_UPDATE_TIME)));
+            return user;
         }
-
-
         return user;
     }
-    
 }
