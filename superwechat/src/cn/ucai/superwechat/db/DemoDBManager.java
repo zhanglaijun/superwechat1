984667f429335c07016ba75b55797075994429cb
zhanglaijun <125682237@qq.com>
2016-07-25 16:11:15 +0800

    更新内存和数据库的用户昵称

--- superwechat/src/cn/ucai/superwechat/db/DemoDBManager.java
+++ superwechat/src/cn/ucai/superwechat/db/DemoDBManager.java
@@ -383,4 +383,14 @@ public class DemoDBManager {
         }
         return user;
     }
+
+    public void updateUserNick(UserAvatar user) {
+        SQLiteDatabase db=dbHelper.getWritableDatabase();
+        ContentValues values=new ContentValues();
+        values.put(UserDao.USER_COLUMN_NAME_NICK, user.getMUserNick());
+        if(db.isOpen()){
+            db.update(UserDao.USER_TABLE_NAME,values,UserDao.USER_COLUMN_NAME_ID+" = ?",
+                    new String[]{user.getMUserName()});
+        }
+    }
 }
