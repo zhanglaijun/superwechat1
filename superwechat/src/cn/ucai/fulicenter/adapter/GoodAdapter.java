2356748ddbc14686fee117b7a48eb17b5413cd05
zhanglaijun <125682237@qq.com>
2016-08-01 19:04:56 +0800

    创建新品的Adapter

---
+++ superwechat/src/cn/ucai/fulicenter/adapter/GoodAdapter.java
@@ -0,0 +1,68 @@
+package cn.ucai.fulicenter.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import cn.ucai.fulicenter.R;
+import cn.ucai.fulicenter.bean.NewGoodBean;
+
+/**
+ * Created by Administrator on 2016/8/1.
+ */
+public class GoodAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>{
+    Context mContext;
+    List<NewGoodBean>mGoodList;
+    GoodViewHolder mGoodViewHolder;
+
+    public GoodAdapter(Context Context, List<NewGoodBean> list) {
+        mContext = Context;
+        mGoodList = new ArrayList<NewGoodBean>();
+        mGoodList.addAll(list);
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        RecyclerView.ViewHolder holder=null;
+        LayoutInflater inflater=LayoutInflater.from(mContext);
+        holder=new GoodViewHolder(inflater.inflate(R.layout.fragment_new_goods,null,false));
+        return  holder;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        if(holder instanceof GoodViewHolder){
+            mGoodViewHolder= (GoodViewHolder) holder;
+            NewGoodBean good=mGoodList.get(position);
+
+            mGoodViewHolder.tvGoodName.setText(good.getGoodsName());
+            mGoodViewHolder.tvGoodPrice.setText(good.getCurrencyPrice());
+        }
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mGoodList.size();
+    }
+    class GoodViewHolder extends RecyclerView.ViewHolder{
+        LinearLayout layout;
+        ImageView ivGoodThumv;
+        TextView tvGoodName,tvGoodPrice;
+        public GoodViewHolder(View itemView) {
+            super(itemView);
+            layout= (LinearLayout) itemView.findViewById(R.id.layout_good);
+            ivGoodThumv= (ImageView) itemView.findViewById(R.id.niv_good_thumb);
+            tvGoodName= (TextView) itemView.findViewById(R.id.tv_good_name);
+            tvGoodPrice= (TextView) itemView.findViewById(R.id.tv_good_price);
+        }
+    }
+}
